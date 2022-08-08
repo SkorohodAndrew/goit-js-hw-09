dda0ef0ebe61c34b6a68ec4896475d279c254f39
Skorohod Andrew <pmisa376@gmail.com>
2022-08-08 13:50:19 +0300

    fix 3 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,44 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  day: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.addEventListener('click', startTimer);
+refs.startBtn.setAttribute('disabled', true);
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    if (selectedDates[0] <= new Date()) {
+      window.alert('Please choose a date in the future');
+      refs.startBtn.setAttribute('disabled', false);
+    } else {
+      refs.startBtn.removeAttribute('disabled', false);
+    }
     console.log(selectedDates[0]);
   },
 };
+
+flatpickr(refs.input, options);
+
+function startTimer() {
+  const startTime = Date.now();
+
+  setInterval(() => {
+    const currentTime = Date.now();
+    console.log(currentTime - startTime);
+  }, 1000);
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -29,7 +58,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
